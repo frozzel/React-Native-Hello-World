c7ab09742aef8b129fa91c4436bf2c8ceec0f25c
Dennis Hickox <frozzel@me.com>
2024-08-16 14:09:31 -0400

    testing new index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,34 +16,35 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hello React Native!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Axios Hubspot Example</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
+        Demonstrates how to connect to an external API using Axios. The app is designed to fetch and display contact information from a HubSpot CRM account. 
+        {/* <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes. */}
+          {/* Press{' '} */}
+          {/* <ThemedText type="defaultSemiBold">
             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
+          </ThemedText>{' '} */}
+          {/* to open developer tools. */}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText type="subtitle">Native Expo Contacts</ThemedText>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+        The Expo Contacts module in React Native allows developers to access and manage the device's contacts on iOS and Android. Using this module, you can query the user's contact list, retrieve detailed information such as names, phone numbers, and emails.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        <ThemedText type="subtitle">Native Expo Maps</ThemedText>
         <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+        The Expo Location module in React Native enables developers to integrate maps and detect the user's current location with ease. By leveraging this module, you can access the user's geographic coordinates and display their position on a map, allowing for real-time location tracking and interactive map experiences.
+          {/* <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          <ThemedText type="defaultSemiBold">app-example</ThemedText>. */}
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
